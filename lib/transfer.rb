066a3c60ec25e53ce02a2fab7dcf2601b4449d4f
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-06 17:17:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
   attr_accessor :sender, :receiver, :status, :amount
-  def initialize(sender,receiver)
+  def initialize(sender,receiver,amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
-
+    @amount
   end
 end

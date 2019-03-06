d7c348fded12747a23681798d11e345cf2f3feb2
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-06 17:47:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,9 +21,9 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
-    else 
+    else
       "Your account is not valid"
 
-    
+
   end
 end

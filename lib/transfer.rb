050ba7b05a59e4db20c9095836bcca0da6acc3f8
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-06 18:02:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
 
   def execute_transaction
     # binding.pry
-    # if (@sender.valid? == true)
+    if (@sender.valid? == true) && (@status == "complete")
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"

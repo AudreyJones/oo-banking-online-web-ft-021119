d39490ee28cc04ef0f698da67b07aa0df1102b32
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-06 18:01:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
     if @status = "complete"
-      return
+      return "No more transactions allowed."
     end
     # elsif (@sender.valid? == false)
     #   "Transaction rejected. Please check your account balance."
